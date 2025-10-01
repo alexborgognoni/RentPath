294ca4d83435f57e66bfeaff49ba3c4223a014c9
alexborgognoni <alex.bor0419@gmail.com>
2025-10-01 22:24:36 +0200

    Fix build error

--- resources/js/components/app-shell.tsx
+++ resources/js/components/app-shell.tsx
@@ -1,4 +1,3 @@
-import { ParallaxBackground } from '@/components/parallax-background';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { SharedData } from '@/types';
 import { usePage } from '@inertiajs/react';
@@ -14,9 +13,6 @@ export function AppShell({ children, variant = 'header' }: AppShellProps) {
     if (variant === 'header') {
         return (
             <div className="relative flex min-h-screen w-full flex-col bg-background lg:h-screen lg:overflow-hidden">
-                <div className="absolute inset-0 min-h-full w-full lg:h-screen">
-                    <ParallaxBackground />
-                </div>
                 <div className="relative z-10">{children}</div>
             </div>
         );
