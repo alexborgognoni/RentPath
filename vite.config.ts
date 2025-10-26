e1fdce672eb9e1a0f6bc2a267143cbdadbca84f7
Alex Borgognoni <alex.borgognoni@deutsche-boerse.com>
2025-10-26 19:33:26 +0100

    Add dev:lan script

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,32 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
-    esbuild: {
-        jsx: 'automatic',
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.tsx'],
+                ssr: 'resources/js/ssr.tsx',
+                refresh: true,
+            }),
+            react(),
+            tailwindcss(),
+            wayfinder({
+                formVariants: true,
+            }),
+        ],
+        esbuild: {
+            jsx: 'automatic',
+        },
+        server: {
+            host: env.VITE_DEV_SERVER_HOST || '127.0.0.1',
+            hmr: env.VITE_HMR_HOST ? {
+                host: env.VITE_HMR_HOST,
+            } : true,
+        },
+    };
 });
